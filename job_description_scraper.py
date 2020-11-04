3da98a41d986d4a8824ebc7549d96450430eda0b
Jerome Wynne <jeromewynne@das-ltd.co.uk>
2020-11-04 11:38:30 +0000

    Add stubs to directories

--- job_description_scraper.py
+++ job_description_scraper.py
@@ -1,1 +1,19 @@
 # -*- utf-8 -*-
+
+# Created: 4th November 2020
+# Author: Jerome Wynne (jeromewynne@das-ltd.co.uk)
+# Environment: wmchack
+# Summary: Scrapes job descriptions from the NHS Jobs
+#          website and writes them to a JSON list.
+
+import requests
+import bs4 as bs
+
+# example URL: https://www.jobs.nhs.uk/xi/vacancy/916247105
+# questions:
+#   how can we identify job description URLs?
+#   how can we extract the information from the page?
+
+base_url = 'https://www.jobs.nhs.uk/xi/vacancy/'
+
+requests.get()
