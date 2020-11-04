5c6cb81f85415d65ecde02d4edff6ede0124cfc3
Jerome Wynne <jeromewynne@das-ltd.co.uk>
2020-11-04 12:05:00 +0000

    Add requirements dir, create write_job_description_to_json function
    write_job_description_to_json currently receives the URL of a job description web page and returns a string containing the job description's fields in JSON (i.e. it does not yet write it to disk)

--- job_description_scraper.py
+++ job_description_scraper.py
@@ -14,6 +14,22 @@ import bs4 as bs
 #   how can we identify job description URLs?
 #   how can we extract the information from the page?
 
-base_url = 'https://www.jobs.nhs.uk/xi/vacancy/'
+def write_job_description_to_json(url: str):
+    ''' Parses a job description web page and writes its fields
+        to a JSON file.
 
-requests.get()
+        Args:
+            url: URL to a page with the same template as
+                 https://www.jobs.nhs.uk/xi/vacancy/916243341.
+
+        Returns:
+            nothing
+    '''
+    soup = bs.BeautifulSoup(requests.get(url).text, 'html.parser')
+    script_tag = soup.find('script', # job description in JSON 
+                            attrs={'id':'jobPostingSchema'})
+    return(script_tag.contents)
+
+url = 'https://www.jobs.nhs.uk/xi/vacancy/916243341'
+script_tag = write_job_description_to_json(url)
+print(script_tag)
